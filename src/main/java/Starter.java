136c9759ce6aa447d500941c1903dd33855d3639
laisk <laisk1@hotmail.com>
2021-03-29 09:00:54 +0300

    stable version

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -7,11 +7,11 @@ import java.io.IOException;
 
 public class Starter {
     public static void main(String[] args) {
-        String pathToFile = "resources/images/invalid.jpg";
+        String pathToFile = "resources/images/valid.jpg";
         String pathToResultFile = "resources/images/result.jpg";
         try{
         String incomingString = Utils.encodedFileToBase64String(pathToFile);
-        String outgoingString = ImageResizer.resizeAndCompressBase64Image(incomingString,1900,400000);
+        String outgoingString = ImageResizer.resizeAndCompressBase64Image(incomingString,1600,400000);
         Utils.writeEncodedImageToFile(outgoingString,pathToResultFile);
         }catch (ImageResizerException | ImageCompressionException | IOException | InvalidJPGException e){
             e.printStackTrace();
